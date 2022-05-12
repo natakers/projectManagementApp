29c3dd01987335bcbb56568bb169d7c08d478ad0
gonzjv <gonzjv@ya.ru>
2022-05-12 11:59:01 +0300

    build: tailwind

--- src/pages/welcome-page.tsx
+++ src/pages/welcome-page.tsx
@@ -1,5 +1,9 @@
 const WelcomePage = () => {
-  return <main>Hello from, Shire!</main>;
+  return (
+    <main className="text-3xl text-lime-700 ">
+      Hello from, Shire!
+    </main>
+  );
 };
 
 export default WelcomePage;
