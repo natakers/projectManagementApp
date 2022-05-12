13142959395349db73f54218db12875ebec989b3
gonzjv <gonzjv@ya.ru>
2022-05-13 00:09:32 +0300

    refactor: welcome page styles

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,7 +3,7 @@ import './layout.css';
 
 const Layout = () => {
   return (
-    <div className="layout">
+    <div className="min-h-screen">
       <Outlet />
     </div>
   );
--- src/pages/welcome-page.tsx
+++ src/pages/welcome-page.tsx
@@ -1,7 +1,32 @@
 const WelcomePage = () => {
+  const DEVS = [
+    { link: 'https://github.com/natakers', githubName: '@natakers' },
+    {
+      link: 'https://github.com/MaxTheGrandMagus',
+      githubName: '@MaxTheGrandMagus',
+    },
+    { link: 'https://github.com/gonzjv', githubName: '@gonzjv' },
+  ];
+
   return (
-    <main className="text-3xl text-lime-700 ">
-      Hello from, Shire!
+    <main className="relative bg-slate-800 min-h-screen items-center text-gray-300 justify-center flex flex-col gap-5 ">
+      <h1 className="text-3xl text-white">effetto</h1>
+      <p className="text-lg">Project Management App</p>
+      <aside className="bottom-10 absolute mt-32 text-lg justify-center items-center flex flex-col gap-5">
+        <p>React2022Q1 | Team75</p>
+        <ul className="flex gap-5">
+          {DEVS.map((dev) => (
+            <li>
+              <a
+                className="font-light underline text-sky-400"
+                href={dev.link}
+              >
+                {dev.githubName}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </aside>
     </main>
   );
 };
