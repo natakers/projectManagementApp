bd2ffa627b754bb391363b2d1721b69c53c20d75
gonzjv <gonzjv@ya.ru>
2022-05-29 00:21:27 +0300

    refactor: styles of add task popup

--- src/components/creationTask.tsx
+++ src/components/creationTask.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react"
-import Input from "../components/input";
-import { TaskCreationProps, TokenProps } from "../components/interfaces";
-import BoardButton, { themes } from "../components/main-route/boardButton";
-import { createTask } from "../store/task/taskSlice";
-import { useAppDispatch } from "../store/store";
-import { useCookies } from "react-cookie";
-import jwt_decode from "jwt-decode"
+import { useState } from 'react';
+import Input from '../components/input';
+import {
+  TaskCreationProps,
+  TokenProps,
+} from '../components/interfaces';
+import BoardButton, {
+  themes,
+} from '../components/main-route/boardButton';
+import { createTask } from '../store/task/taskSlice';
+import { useAppDispatch } from '../store/store';
+import { useCookies } from 'react-cookie';
+import jwt_decode from 'jwt-decode';
 
-const TaskCreation = ({toggleWindow, colId, order}: TaskCreationProps) => {
+const TaskCreation = ({
+  toggleWindow,
+  colId,
+  order,
+}: TaskCreationProps) => {
   const [cookie] = useCookies(['user']);
-  const decodedUser: TokenProps = jwt_decode(cookie.user)
-  const userId = decodedUser.userId
-  const boardId = localStorage.getItem('boardId')
-  
+  const decodedUser: TokenProps = jwt_decode(cookie.user);
+  const userId = decodedUser.userId;
+  const boardId = localStorage.getItem('boardId');
+
   const [formData, setFormData] = useState({
     title: '',
     description: '',
@@ -24,13 +33,11 @@ const TaskCreation = ({toggleWindow, colId, order}: TaskCreationProps) => {
     setFormData((prevState) => ({
       ...prevState,
       [event.target.name]: event.target.value,
-    }))
+    }));
   };
-  
+
   const taskData = {
-    task: {title,
-    description,
-    userId},
+    task: { title, description, userId },
     colId,
     boardId,
   };
@@ -44,26 +51,53 @@ const TaskCreation = ({toggleWindow, colId, order}: TaskCreationProps) => {
   };
 
   return (
-    <section className={`flex createModal max-w-sm flex-col absolute rounded z-20 bg-slate-50 p-4 inset-y-80 inset-x-0 m-auto items-center`}>
-      <div className="logo__container w-full flex flex-col justify-center items-center gap-3">
-        <p className="title text-center font-bold text-3xl text-gray-900 mb-6">
-          Create new task
-        </p>
-      </div>
+    <section className="flex w-52 flex-col absolute rounded z-20 bg-sky-900 border border-sky-500 h-20 p-2 top-0 items-center">
       <form
         onSubmit={onSubmit}
-        className="form w-2/4 flex flex-col justify-center items-center gap-6"
+        className="form w-full flex flex-col justify-start items-start gap-6"
       >
-        <Input value={title} name='title' type='text' placeholder='Name of task' id='title' onChange={onChange} />
-        <Input value={description} name='description' type='text' placeholder='Description of task' id='description' onChange={onChange} />
+        <label htmlFor="title w-full">
+          <input
+            className="w-full bg-transparent"
+            value={title}
+            name="title"
+            onChange={onChange}
+            placeholder="Type task title here..."
+            type="text"
+            required
+          />
+        </label>
+        {/* <Input
+          value={title}
+          name="title"
+          type="text"
+          placeholder="Name of task"
+          id="title"
+          onChange={onChange}
+        />
+        <Input
+          value={description}
+          name="description"
+          type="text"
+          placeholder="Description of task"
+          id="description"
+          onChange={onChange}
+        />
         <div className="flex w-full justify-around ">
-          <BoardButton themes={themes.dark} type="submit" text="Create" />
-          <BoardButton themes={themes.dark} text='Back' onClick={toggleWindow} />
-        </div>
+          <BoardButton
+            themes={themes.dark}
+            type="submit"
+            text="Create"
+          />
+          <BoardButton
+            themes={themes.dark}
+            text="Back"
+            onClick={toggleWindow}
+          />
+        </div> */}
       </form>
     </section>
   );
 };
 
 export default TaskCreation;
-
