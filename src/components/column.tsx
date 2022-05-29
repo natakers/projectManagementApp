83ff175e9179bec9ac1f588c89335622d98178da
Max <veyronmclord@gmail.com>
2022-05-29 15:30:36 +0600

    feat: add dnd for columns

--- src/components/column.tsx
+++ src/components/column.tsx
@@ -7,6 +7,7 @@ import TaskCreation from './creationTask';
 import TrashIcon from '../assets/icons/trash.icon';
 import { deleteColumn } from '../store/columns/colSlice';
 import { AppState, useAppDispatch, useAppSelector } from '../store/store';
+
 export interface ColumnProps {
   colId: string,
   boardId: string,
@@ -32,40 +33,37 @@ const Column = ({title, id, order, tasks, taskClick}: ColumnTaskProps) => {
       console.log(colTasks.columns);
     }
   }
-  
-    
 
-
-  
-  // const col = colTasks.columns.find((el) => el.id === d);
   
   return (
-  <article
-    key={id}
-    className="overflow-y-auto h-full bg-slate-700"
+    <article
+      key={id}
+      className="overflow-y-auto h-full bg-slate-700"
     >
       <div className='flex justify-center align-baseline'>
         <h4 className='m-3'>{title}</h4>
         <div className=" relative flex m-3 items-center cursor-pointer hover:bg-slate-500 " onClick={toggleAddTask}>
-          { visibleAddTask &&
-          <div className='flex flex-col absolute top-full right-0 bg-sky-800'>
-            <BoardButton onClick={toggeTaskWindow} text='+ Add task' themes={themes.grey} />
+          { 
+            visibleAddTask &&
+            <div className='flex flex-col absolute top-full right-0 bg-sky-800'>
+              <BoardButton onClick={toggeTaskWindow} text='+ Add task' themes={themes.grey} />
               <button className={themes.grey} onClick={() => handleColumnDelete(id)}>
                 <TrashIcon />
               </button>
-          </div>}
-        <DotsIcon />
+            </div>
+          }
+          <DotsIcon />
         </div>
       </div>
-    <div className='flex flex-col '>
-    {tasks && 
+      <div className='flex flex-col '>
+        {
+          tasks && 
             tasks.map((task) => (
-             <Task taskClick={taskClick} key={task.id} task={task} />
-          ))}
-    </div>
-    <div>
-    </div>
-    { isOpenTaskWin && <TaskCreation  order={order} colId={id} toggleWindow={toggeTaskWindow} />}
+              <Task taskClick={taskClick} key={task.id} task={task} />
+            ))
+        }
+      </div>
+      { isOpenTaskWin && <TaskCreation  order={order} colId={id} toggleWindow={toggeTaskWindow} />}
     </article>
   )
 }
