a55c145d51b90011bbd2e0fc918c4b87b0f4478c
gonzjv <gonzjv@ya.ru>
2022-05-31 00:47:40 +0300

    refactor: task and columns styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import Layout from './components/layout';
 import WelcomePage from './pages/welcome-page';
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,53 +15,40 @@ import { getCookie } from './helpers/cookie';
 import { useCookies } from 'react-cookie';
 
 const App = () => {
-  // const fetchApiData = async () => {
-  //   const headers = new Headers({
-  //     Authorization:
-  //       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0NGNmOTZkMi01OGZjLTRlMGMtOTZkOS05YWM0MjhkNGQ0OTUiLCJsb2dpbiI6InVzZXIwMDEiLCJpYXQiOjE2NTIwMDMyMTF9.EUlvrrs0Hl7wq1o-vkW5eh710CeNmhTfivk8aYkO43I',
-  //   });
-  //   const options = {
-  //     method: 'GET',
-  //     headers,
-  //     // mode: 'no-cors' as RequestMode,
-  //   };
-
-  //   const apiData = await fetch(
-  //     'https://frozen-depths-66382.herokuapp.com/users',
-  //     options
-  //   );
-  //   const parsedApiData = await apiData.json();
-  //   console.log('users from API', parsedApiData);
-  // };
-
-
-  // useEffect(() => {
-  //   fetchApiData();
-  // });
-  const locale = getCookie('lang') || LOCALES.ENGLISH
+  const locale = getCookie('lang') || LOCALES.ENGLISH;
   const [cookie, setCookie] = useCookies(['lang']);
-  const handleChange = (e: { target: { value: string; }}) => {    
-  setCookie('lang', e.target.value)
-  console.log(cookie);
-  
-}
+  const handleChange = (e: { target: { value: string } }) => {
+    setCookie('lang', e.target.value);
+    console.log(cookie);
+  };
 
   return (
-    <IntlProvider messages={messages[locale]} locale={locale} defaultLocale={LOCALES.ENGLISH}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout handleChange={handleChange}  currentLocale={cookie.lang} />}>
-          <Route index element={<WelcomePage />} />
-          <Route path="/signin" element={<SigninPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/board" element={<BoardPage />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <IntlProvider
+      messages={messages[locale]}
+      locale={locale}
+      defaultLocale={LOCALES.ENGLISH}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout
+                handleChange={handleChange}
+                currentLocale={cookie.lang}
+              />
+            }
+          >
+            <Route index element={<WelcomePage />} />
+            <Route path="/signin" element={<SigninPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/board" element={<BoardPage />} />
+            <Route path="/edit-profile" element={<EditProfile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </IntlProvider>
-    
   );
 };
 
--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,31 +1,35 @@
-import { useState } from "react";
-import DotsIcon from "../../assets/icons/dotsIcon";
-import TrashIcon from "../../assets/icons/trash.icon";
-import { useAppDispatch } from "../../store/store";
-import { chooseColId, chooseTaskId, deleteTask } from "../../store/task/taskSlice";
-import { TaskDelProps, TaskProps } from "../interfaces"
-import  { themes } from "../main-route/boardButton";
+import { useState } from 'react';
+import DotsIcon from '../../assets/icons/dotsIcon';
+import TrashIcon from '../../assets/icons/trash.icon';
+import { useAppDispatch } from '../../store/store';
+import {
+  chooseColId,
+  chooseTaskId,
+  deleteTask,
+} from '../../store/task/taskSlice';
+import { TaskDelProps, TaskProps } from '../interfaces';
+import { themes } from '../main-route/boardButton';
 
-const Task = ({task, taskClick, columnId }: TaskProps) => {
+const Task = ({ task, taskClick, columnId }: TaskProps) => {
   const dispatch = useAppDispatch();
   const [visibleAddTask, setVisibleAddTask] = useState(false);
-  const toggleDelTask = (event: { stopPropagation: () => void; }) => {
+  const toggleDelTask = (event: { stopPropagation: () => void }) => {
     event.stopPropagation();
-    setVisibleAddTask(!visibleAddTask)
+    setVisibleAddTask(!visibleAddTask);
   };
-  const boardId = localStorage.getItem('boardId')
+  const boardId = localStorage.getItem('boardId');
   let treeId: TaskDelProps;
   if (boardId) {
     treeId = {
       boardId: boardId,
       colId: columnId,
-      taskId: task.id
-    }
+      taskId: task.id,
+    };
   }
 
   const handleTaskDelete = () => {
-    dispatch(deleteTask(treeId)); 
-  }
+    dispatch(deleteTask(treeId));
+  };
 
   const openTask = () => {
     if (taskClick) {
@@ -33,25 +37,34 @@ const Task = ({task, taskClick, columnId }: TaskProps) => {
       dispatch(chooseTaskId(task));
       dispatch(chooseColId(columnId));
     }
-  }
+  };
   return (
-    <div className="bg-sky-500 m-2 p-2 hover:bg-slate-600" onClick={openTask}>
-      <div className="flex justify-between ">
-        <h2>Title: {task.title}</h2>
-        <div className=" relative flex items-center cursor-pointer hover:bg-slate-500 " onClick={toggleDelTask}>
-          { visibleAddTask && task.id !== undefined &&
-          <div className='flex flex-col absolute top-full right-0 bg-sky-800'>
-              <button className={themes.grey} onClick={handleTaskDelete}>
+    <div
+      className="bg-slate-700 border border-slate-600 rounded-md m-2 p-2 hover:border hover:border-slate-500"
+      onClick={openTask}
+    >
+      <div className="flex justify-between text-orange-200">
+        <h2>{task.title}</h2>
+        <div
+          className=" relative flex items-center cursor-pointer hover:bg-slate-500 "
+          onClick={toggleDelTask}
+        >
+          {visibleAddTask && task.id !== undefined && (
+            <div className="flex flex-col absolute top-full right-0 bg-sky-800">
+              <button
+                className={themes.grey}
+                onClick={handleTaskDelete}
+              >
                 <TrashIcon />
               </button>
-          </div>}
-        <DotsIcon />
+            </div>
+          )}
+          <DotsIcon />
         </div>
       </div>
-      <div>Description: {task.description}</div>
-      
+      <div>{task.description}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
