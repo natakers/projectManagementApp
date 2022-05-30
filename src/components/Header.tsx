4d55e4d2b274b6e8062f68333c58ff44b76cf477
natakers <n.kersnovskaya@gmail.com>
2022-05-30 23:12:40 +0300

    feat: add burger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEventHandler, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { AppState, useAppDispatch } from '../store/store';
@@ -8,32 +8,34 @@ import { useCookies } from 'react-cookie';
 import Logo from './logo';
 import BoardButton, { themes } from './main-route/boardButton';
 import BoardCreation from '../pages/createBoard';
-import jwt_decode from 'jwt-decode';
+import jwt_decode from 'jwt-decode'
 import { TokenProps } from './interfaces';
 import userImg from '../assets/images/sample-avatar.jpg';
+// import { FormattedMessage } from 'react-intl'
 import { LOCALES } from '../i18n/locales';
+import Menu from '../assets/icons/menu';
 
 export type HeaderProps = {
-  currentLocale: string;
-  handleChange: ({
-    target: { value },
-  }: {
-    target: { value: string };
-  }) => void;
+  currentLocale: string,
+  handleChange: ({ target: { value } }: {target: {value: string }}) => void,
 };
 
-const Header = ({ currentLocale, handleChange }: HeaderProps) => {
+
+const Header = ({currentLocale, handleChange }: HeaderProps) => {
   const [sticky, setSticky] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [burger, setBurger] = useState(false);
+  const [width, setWidth] = useState(0);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const [cookie, setCookie, removeCookie] = useCookies(['user']);
-  const decodedUser: TokenProps = jwt_decode(cookie.user);
-
+  const decodedUser: TokenProps = jwt_decode(cookie.user)
+  // const ref = React.useRef();
+  const [isNavOpen, setIsNavOpen] = useState(false)
   const { userDetails } = useSelector(
     (state: AppState) => state.user
   );
-
+  const inputEl = useRef(null);
   const onLogout = () => {
     dispatch(reset());
     dispatch(resetUser());
@@ -56,40 +58,43 @@ const Header = ({ currentLocale, handleChange }: HeaderProps) => {
         setSticky(false);
       }
     };
+    const handleWidthHeader = () => {
+      if (window.innerWidth <= 640) {
+        setBurger(true);
+      } else {
+        setBurger(false);
+      }
+    };
     window.addEventListener('scroll', handleStickyHeader);
+    window.addEventListener('resize', handleWidthHeader);
     return () => {
       window.removeEventListener('scroll', handleStickyHeader);
+      window.removeEventListener('resize', handleWidthHeader);
     };
-  }, [
-    userDetails.name,
-    userDetails.login,
-    dispatch,
-    decodedUser.userId,
-  ]);
+  }, [userDetails.name, userDetails.login, dispatch, decodedUser.userId, width]);
 
   const languages = [
     { name: 'EN', code: LOCALES.ENGLISH },
     { name: 'RU', code: LOCALES.RUSSIAN },
-  ];
+  ]
+
+  const openMenu = () => {
+    setIsNavOpen(!isNavOpen)
+  }
+console.log(burger);
 
   return (
-    <header
-      className={`${
-        sticky ? 'header--sticky' : 'h-24'
-      } bg-slate-800 w-full flex justify-between items-center px-6 py-6 border-b border-b-slate-600 text-gray-300`}
-    >
-      <div className="logo">
+    <header  className={`${sticky ? 'header--sticky' : 'h-24'} relative bg-slate-800 w-full flex justify-between items-center px-6 py-6 border-b border-b-slate-600 text-gray-300`}>
+      <div ref={inputEl}  className="logo">
         <Link to="/main">
           <Logo />
         </Link>
       </div>
-      <div className="nav__list flex justify-between items-center gap-4">
-        <>
-          <BoardButton
-            themes={themes.light}
-            text="boardCreationBtn"
-            onClick={toggleWindow}
-          />
+      
+        <div className='flex justify-between'>
+      { <div className={`${(burger && isNavOpen)? "hidden" : 'visible'} nav__list z-20  absolute bg-slate-700 p-2 top-12 right-10 flex flex-col justify-between items-center gap-4 sm:bg-slate-800 sm:flex-row sm:static `}>
+        
+          <BoardButton themes={themes.light} text="boardCreationBtn" onClick={toggleWindow} />
           <Link to="/edit-profile">
             <BoardButton themes={themes.light} text="edit" />
           </Link>
@@ -99,23 +104,18 @@ const Header = ({ currentLocale, handleChange }: HeaderProps) => {
             onClick={onLogout}
           />
           <div className="switch">
-            <select
-              className="text-white bg-sky-600 mr-4"
-              onChange={handleChange}
-              value={currentLocale}
-            >
-              {languages.map(({ name, code }) => (
-                <option
-                  className="text-white bg-slate-800"
-                  key={code}
-                  value={code}
-                >
-                  {name}
-                </option>
-              ))}
-            </select>
+          <select className='text-white bg-sky-600 mr-4' onChange={handleChange}  value={currentLocale}>
+            {languages.map(({ name, code }) => (
+              <option className='text-white bg-slate-800' key={code}  value={code}>
+                {name}
+              </option>
+            ))}
+          </select>
           </div>
-          <div className="nav__user w-full flex flex-row justify-center items-center gap-2">
+          
+
+      </div> }
+      <div className="nav__user w-full flex flex-row justify-center items-center gap-2">
             <img
               src={userImg}
               alt="user avatar"
@@ -123,9 +123,11 @@ const Header = ({ currentLocale, handleChange }: HeaderProps) => {
             />
             <span>{userDetails.login}</span>
           </div>
-        </>
       </div>
-      {isOpen && <BoardCreation toggleWindow={toggleWindow} />}
+      {burger && <div onClick={openMenu} >
+          <Menu />
+        </div>} 
+      { isOpen && <BoardCreation toggleWindow={toggleWindow} />}
     </header>
   );
 };
